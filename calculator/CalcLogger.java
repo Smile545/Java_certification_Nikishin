1b020f9fb85de9d1d17fac268e613d5074351247
Bitrix24 3 <dialb3@dialweb.local>
2023-07-24 16:24:38 +0300

    V3

---
+++ calculator/CalcLogger.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class CalcLogger implements Loggable{
+    @Override
+    public void saveLog(String str) {
+        System.out.println("Log message: " + str);
+    }
+}
